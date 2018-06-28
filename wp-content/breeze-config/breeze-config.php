f59d4be5dd10bf50972d734e7335520574de126a
Kevin S. Cooke <kevin@searchandebfound.com>
2018-06-28 15:39:11 +0000

    moving the files onto Github for mass replication

---
+++ wp-content/breeze-config/breeze-config.php
@@ -0,0 +1,23 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+return array (
+  'homepage' => 'http://wordpress-84563-543239.cloudwaysapps.com',
+  'cache_options' =>
+  array (
+    'breeze-active' => '1',
+    'breeze-ttl' => '',
+    'breeze-minify-html' => '0',
+    'breeze-minify-css' => '0',
+    'breeze-minify-js' => '0',
+    'breeze-gzip-compression' => '1',
+    'breeze-desktop-cache' => '1',
+    'breeze-browser-cache' => '1',
+    'breeze-mobile-cache' => '1',
+    'breeze-disable-admin' => '1',
+    'breeze-display-clean' => '1',
+  ),
+  'disable_per_adminuser' => '1',
+  'exclude_url' =>
+  array (
+  ),
+);
---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'co2YKu53EEzUUQ1FGf0v1jzQouJ8jEMpcsSEJpycXo42pgWRPNGLN0TariyKgdD3');
+define('SECURE_AUTH_KEY',  '5tFpF0Cs5fE5ngCI6mF2dbfCNd8w2nuzmdnS81jWVdadK26t6Ft2e4VBBPSTB29B');
+define('LOGGED_IN_KEY',    '6SFCPg5Aav0Dh4QjrwL8Q9d1rKrjaiGIHSBrJH0ayKpGcK6ut81f8qNVEqEdWHvj');
+define('NONCE_KEY',        'B6MnhSXEuSJL9z9xd3dT53qA7f5T3X73HLPRJH7BKnn5gAKAMjyxS0E5Vm6qVVGs');
+define('AUTH_SALT',        'o81xVRRYq8IrQpFX6AUxuhrXfugRUiWU4Tw36Ev0rcHHxv7dj4BcTaFuTIevbI0n');
+define('SECURE_AUTH_SALT', 'M6oYv2YngbdWTGLGW3qL2H8sC0IF3zEQoIDNRDzXXoSppjqxo0fu1pFW9toIcwTi');
+define('LOGGED_IN_SALT',   'CTAdIP9Et1ETqz3M5r7TDN5p46X0eoTuBu7eaufBcsXn1cFyhA1LPiNSapx8YdxC');
+define('NONCE_SALT',       'KGtIbyzMU9Bi2AVMKo6irzCMtwttSaTsY13MzvUbncTvsuxfd951IjP3Kd5FRN5e');
